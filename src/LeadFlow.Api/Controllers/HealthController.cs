d78bedaeeabaafb0350e8cd4563c1bbb4537666f
jjforsyth15 <jjforsyth15@gmail.com>
2026-08-06 17:10:43 -0700

    Initialize ASP.NET Core solution and test project

---
+++ src/LeadFlow.Api/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeadFlow.Api.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetHealth()
+    {
+        return Ok(new
+        {
+            status = "healthy",
+            timestamp = DateTimeOffset.UtcNow
+        });
+    }
+}
+
+    
---
+++ tests/LeadFlow.Tests/SmokeTests.cs
@@ -0,0 +1,11 @@
+namespace LeadFlow.Tests
+{
+    public class SmokeTests
+    {
+        [Fact]
+        public void TestProjectRuns()
+        {
+            Assert.True(true);
+        }
+    }
+}
